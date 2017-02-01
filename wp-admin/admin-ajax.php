1e574330747546e4b32facdd432419bb7cd605f7
Apurba Paul <apurba.paul@rci.rogers.ca>
2017-02-01 09:51:41 -0500

    First Commit

--- wp-admin/admin-ajax.php
+++ wp-admin/admin-ajax.php
@@ -13,10 +13,13 @@
  *
  * @since 2.1.0
  */
+
+/*
 define( 'DOING_AJAX', true );
 if ( ! defined( 'WP_ADMIN' ) ) {
 	define( 'WP_ADMIN', true );
 }
+*/
 
 /** Load WordPress Bootstrap */
 require_once( dirname( dirname( __FILE__ ) ) . '/wp-load.php' );
